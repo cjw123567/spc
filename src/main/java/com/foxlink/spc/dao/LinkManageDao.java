84a3f80a6ac2c00d68877cab11c21e4d0bbbed8b
asd3354389 <401973312@qq.com>
2019-01-18 10:31:05 +0800

    LinkManage修改

---
+++ src/main/java/com/foxlink/spc/dao/LinkManageDao.java
@@ -0,0 +1,10 @@
+package com.foxlink.spc.dao;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+
+@Repository("linkManageDao")
+public class LinkManageDao {
+	private static Logger logger=Logger.getLogger(LinkManageDao.class);
+
+}
